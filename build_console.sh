d18a58f5b7615d13d8f3479a5804eb9e658c7f3f
wangji <genius840215@163.com>
2018-08-02 16:29:51 +0800

    修改前端部署方式

--- build_console.sh
+++ build_console.sh
@@ -17,3 +17,12 @@ else
   echo "npm install fail"
   exit 1
 fi
+
+if [ $? -eq 0 ]; then
+  echo "move dist to /www"
+  mv dist /www/peckergo
+  echo "move complete"
+else
+  echo "move fail"
+  exit 1
+fi
